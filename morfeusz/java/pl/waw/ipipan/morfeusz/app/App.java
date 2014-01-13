e7a9535e26e8f9dcb9a591dc8f5bfe99aa4fd7b7
Michał Lenart <michall@ipipan.waw.pl>
2014-01-13 16:02:46 +0000

    - praca nad paczkowaniem pythona do .deb - porządki w paczkowaniu Javy (wyrzucenie mavenowej maszynerii, użycie tylko cmake-owego add_jar)
    git-svn-id: svn://svn.nlp.ipipan.waw.pl/morfeusz/morfeusz@70 ff4e3ee1-f430-4e82-ade0-24591c43f1fd

---
+++ morfeusz/java/pl/waw/ipipan/morfeusz/app/App.java
@@ -0,0 +1,22 @@
+package pl.waw.ipipan.morfeusz.app;
+
+import pl.waw.ipipan.morfeusz.Morfeusz;
+import pl.waw.ipipan.morfeusz.ResultsIterator;
+
+/**
+ * Hello world!
+ *
+ */
+public class App {
+
+    public static void main(String[] args) {
+        System.err.println("Working Directory = " +
+              System.getProperty("user.dir"));
+        System.err.println("java.library.path="+System.getProperty("java.library.path"));
+        Morfeusz morfeusz = new Morfeusz();
+        ResultsIterator it = morfeusz.analyze("Ala ma kota i żółć.");
+        while (it.hasNext()) {
+            System.out.println(MorfeuszUtils.getInterpretationString(it.getNext()));
+        }
+    }
+}
