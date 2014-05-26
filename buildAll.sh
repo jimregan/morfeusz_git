94d94779a43b7b9aba6cb6424da60b3b7188ce41
Michał Lenart <michall@ipipan.waw.pl>
2014-05-26 16:40:12 +0000

    poprawki w skrypcie buildAll.sh, próba usunięcia części błędów lintiana
    git-svn-id: svn://svn.nlp.ipipan.waw.pl/morfeusz/morfeusz@200 ff4e3ee1-f430-4e82-ade0-24591c43f1fd

--- buildAll.sh
+++ buildAll.sh
@@ -4,6 +4,7 @@ set -e -o pipefail
 
 export CROSSMORFEUSZ_ROOT="$1"
 export INPUT_DICTIONARIES="$2"
+export VERSION_SUFFIX="$3"
 
 function build {
     set -e -o pipefail
@@ -12,12 +13,12 @@ function build {
     shift
     shift
     targets=$@
-
+    
     srcDir=`pwd`
     buildDir=buildall/$os-$arch
-    targetDir=$srcDir/target/$os-$arch
+    targetDir=$srcDir/target
     toolchain=$srcDir/morfeusz/Toolchain-$os-$arch.cmake
-
+    
     echo "Will use $toolchain toolchain"
 
     rm -rf $buildDir
@@ -26,10 +27,11 @@ function build {
     mkdir -p $targetDir
     cd $buildDir
     cmake -D CROSSMORFEUSZ_ROOT=$CROSSMORFEUSZ_ROOT \
-	-D CMAKE_TOOLCHAIN_FILE=$toolchain \
-	-D TARGET_DIR=$targetDir \
-	-D INPUT_DICTIONARIES=$INPUT_DICTIONARIES \
-	$srcDir
+        -D CMAKE_TOOLCHAIN_FILE=$toolchain \
+        -D TARGET_DIR=$targetDir \
+        -D INPUT_DICTIONARIES=$INPUT_DICTIONARIES \
+        -D VERSION_SUFFIX=$VERSION_SUFFIX \
+        $srcDir 2>&1
     echo "building $toolchain" >&2
     make
     make $targets
@@ -42,23 +44,23 @@ function build {
     cd "$srcDir"
 }
 
+function log {
+    os=$1
+    arch=$2
+    stdbuf -oL sed -e $"s/^/$os-$arch:\t/" | tee "log/$os-$arch.out"
+}
+
 export -f build
+export -f log
 
 rm -rf log
 mkdir -p log
-#{
-#    echo "build Linux amd64 package package-java package-python package-builder &> log/linux-amd64.out"
-#    echo "LDFLAGS=-m32;CFLAGS=-m32;CXXFLAGS=-m32 build Linux i386 package package-java package-builder &> log/linux-i386.out"
-#    echo "build Windows amd64 package package-java package-builder &> log/windows-amd64.out"
-#    echo "build Windows i386 package package-java package-builder &> log/windows-i386.out"
-#    echo "build Darwin amd64 package package-builder package-java &> log/darwin-amd64.out"
-#} | xargs --null | 
 
 parallel -j5 bash -c -- \
-	"build Linux amd64 package package-java package-python package-builder 2>&1 | tee log/linux-amd64.out" \
-	"LDFLAGS=-m32;CFLAGS=-m32;CXXFLAGS=-m32 build Linux i386 package package-java package-builder 2>&1 | tee log/linux-i386.out" \
-	"build Windows amd64 package package-java package-builder 2>&1 | tee log/windows-amd64.out" \
-	"build Windows i386 package package-java package-builder 2>&1 | tee log/windows-i386.out" \
-	"build Darwin amd64 package package-builder package-java 2>&1 | tee log/darwin-amd64.out"
+    "build Linux amd64 package package-java package-python package-builder 2>&1 | log Linux amd64" \
+    "LDFLAGS=-m32;CFLAGS=-m32;CXXFLAGS=-m32 build Linux i386 package package-java 2>&1 | log Linux i386" \
+    "build Windows amd64 package package-java 2>&1 | log Windows amd64" \
+    "build Windows i386 package package-java 2>&1 | log Windows i386" \
+    "build Darwin amd64 package package-java 2>&1 | log Darwin amd64"
 
 
